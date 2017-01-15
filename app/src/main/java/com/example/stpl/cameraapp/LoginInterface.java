18f0e26b93a0754f1f0b7cd4516a6cbdc1a76ac0
karandeep singh <karan.truth@gmail.com>
2017-01-15 23:57:09 +0530

    removed extra gridview

---
+++ app/src/main/java/com/example/stpl/cameraapp/LoginInterface.java
@@ -0,0 +1,11 @@
+package com.example.stpl.cameraapp;
+
+/**
+ * Created by stpl on 1/11/2017.
+ */
+
+public interface LoginInterface {
+    void onSuccess();
+
+    void onFailure();
+}
---
+++ app/src/main/java/com/example/stpl/cameraapp/models/MediaDetails.java
@@ -0,0 +1,64 @@
+package com.example.stpl.cameraapp.models;
+
+import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+
+public class MediaDetails implements Parcelable {
+    public static final Creator<MediaDetails> CREATOR = new Creator<MediaDetails>() {
+        @Override
+        public MediaDetails createFromParcel(Parcel in) {
+            return new MediaDetails(in);
+        }
+
+        @Override
+        public MediaDetails[] newArray(int size) {
+            return new MediaDetails[size];
+        }
+    };
+    private Bitmap image;
+    private String filePath;
+    private String mediaType;
+    private boolean isChecked=false;
+
+    public MediaDetails(Bitmap image, String filePath, String type) {
+        this.image = image;
+        this.filePath = filePath;
+        mediaType = type;
+    }
+
+    private MediaDetails(Parcel in) {
+        filePath = in.readString();
+
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+    public void toggleChecked(){
+        isChecked=!isChecked;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(filePath);
+    }
+}
